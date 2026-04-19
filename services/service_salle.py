c0803f872ce4fafeefbbab62a8cb336eeb87e472
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-04-19 03:50:31 -0400

    Ajout de la methode modifier_salle dans  la classe SeviceSalle

--- services/service_salle.py
+++ services/service_salle.py
@@ -15,3 +15,13 @@ class ServiceSalle:
             return False, "Une salle avec ce code existe déjà."
         self.dao_salle.insert_salle(salle)
         return True, "Salle ajoutée avec succès."
+    def modifier_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or salle.capacite is None:
+            return False, "Tous les champs sont obligatoires."
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1."
+        salle_existante = self.dao_salle.get_salle(salle.code)
+        if not salle_existante:
+            return False, "La salle à modifier n'existe pas."
+        self.dao_salle.update_salle(salle)
+        return True, "Salle modifiée avec succès."
