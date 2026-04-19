34adff3fdbd699d0bc7c8cf6ac4c246ba33f7967
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-04-18 20:50:52 -0400

    Ajout de la methode delete_salle  dans Class DataSalle

--- data /dao_salle.py
+++ data /dao_salle.py
@@ -46,3 +46,11 @@ class DataSalle:
         curseur.close()
         connexion.close()
 
+    def delete_salle(self, code):
+        connexion = self.get_connection()
+        curseur = connexion.cursor()
+        requete = "DELETE FROM salle WHERE code = %s"
+        curseur.execute(requete, (code,))
+        connexion.commit()
+        curseur.close()
+        connexion.close()
