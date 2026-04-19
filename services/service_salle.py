8c52a8d00ffe57f00e44905c4870104be807bd94
Saliou Barry <salioubarry@MacBookPro-de-Saliou.local>
2026-04-19 03:48:24 -0400

    Ajout de la methode ajouter_salle dans  la classe SeviceSalle

--- services/service_salle.py
+++ services/service_salle.py
@@ -5,3 +5,13 @@ class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
 
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or salle.capacite is None:
+            return False, "Tous les champs sont obligatoires."
+        if int(salle.capacite) < 1:
+            return False, "La capacité doit être supérieure ou égale à 1."
+        salle_existante = self.dao_salle.get_salle(salle.code)
+        if salle_existante:
+            return False, "Une salle avec ce code existe déjà."
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutée avec succès."
